972de645bd10f0b230e2d353473b7e74623467de
Hasan Cil <79486356+hasancil41@users.noreply.github.com>
2024-12-01 20:54:25 -0500

    Add files via upload

--- 1_top_paying_job.sql
+++ 1_top_paying_job.sql
@@ -1,12 +1,13 @@
 /*
-Question: What are the top-paying Senior Data Scientist remote jobs?
-- Identify the top 10 highest-paying Senior Data Scientist roles that are available remotely
-- Focuses on job postings with specified salaries (remove nulls)
--  Include company names of top 10 roles
-- Why? Highlight the top-paying opportunities for Senior Data Scientist, offering insights into employment options and location flexibility.
-*/
-
-SELECT name as company_name,
+ Question: What are the top-paying Senior Data Scientist remote jobs?
+ - Identify the top 10 highest-paying Senior Data Scientist roles that are available remotely
+ - Focuses on job postings with specified salaries (remove nulls)
+ -Also focus job posted date for first QUARTER
+ -  Include company names of top 10 roles
+ - Why? Highlight the top-paying opportunities for Senior Data Scientist, offering insights into employment options and location flexibility.
+ */
+SELECT job_posted_date::DATE,
+    name as company_name,
     job_id,
     job_title_short,
     job_location,
@@ -18,7 +19,9 @@ WHERE salary_year_avg is NOT NULL
     AND job_title_short = 'Senior Data Scientist'
     AND job_work_from_home IS TRUE
     AND job_location = 'Anywhere'
+    AND EXTRACT(
+        QUARTER
+        FROM job_posted_date
+    ) = 1
 ORDER BY salary_year_avg DESC
 LIMIT 10
-
-
